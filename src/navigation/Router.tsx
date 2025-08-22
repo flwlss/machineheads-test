04910b126a6442b4f2b9c758165c57a725c1204a
Dmitry <dimaandreev1337@gmail.com>
2025-08-22 15:27:36 +0700

    added lazy loading components

--- src/navigation/Router.tsx
+++ src/navigation/Router.tsx
@@ -1,23 +1,28 @@
 import { Switch, Redirect } from "react-router-dom";
 import { PATHS } from "./paths";
-import AuthPage from "../pages/AuthPage";
-import AuthorsPage from "../pages/AuthorsPage";
-import PostsPage from "../pages/PostsPage";
-import TagsPage from "../pages/TagsPage";
 import PrivateRoute from "./PrivateRoute";
-import PublicRoute from "./PublickRoute";
+import PublicRoute from "./PublicRoute";
+import { lazy, Suspense } from "react";
+import { Spin } from "antd";
+
+const AuthPage = lazy(() => import("../pages/AuthPage"));
+const PostsPage = lazy(() => import("../pages/PostsPage"));
+const AuthorsPage = lazy(() => import("../pages/AuthorsPage"));
+const TagsPage = lazy(() => import("../pages/TagsPage"));
 
 const Router = () => {
   return (
-    <Switch>
-      <PublicRoute path={PATHS.AUTH} component={AuthPage} />
+    <Suspense fallback={<Spin fullscreen />}>
+      <Switch>
+        <PublicRoute path={PATHS.AUTH} component={AuthPage} />
 
-      <PrivateRoute path={PATHS.POSTS} component={PostsPage} />
-      <PrivateRoute path={PATHS.AUTHORS} component={AuthorsPage} />
-      <PrivateRoute path={PATHS.TAGS} component={TagsPage} />
+        <PrivateRoute path={PATHS.POSTS} component={PostsPage} />
+        <PrivateRoute path={PATHS.AUTHORS} component={AuthorsPage} />
+        <PrivateRoute path={PATHS.TAGS} component={TagsPage} />
 
-      <Redirect to={PATHS.AUTH} />
-    </Switch>
+        <Redirect to={PATHS.AUTH} />
+      </Switch>
+    </Suspense>
   );
 };
 
